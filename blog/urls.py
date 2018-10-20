0c0f52322766b3791160d713ab9f1bb14ef6340e
type89 <zong1yuan2@gmail.com>
2018-10-20 23:56:12 +0900

    Change to PATH

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,19 @@
 from django.conf.urls import include, url
 from . import views
+from django.contrib import admin
+#from django.urls import path
+from django.urls import include, path
 
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
+    #url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    #url(r'^post/new/$', views.post_new, name='post_new'),
+]
+
+
+"""
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
